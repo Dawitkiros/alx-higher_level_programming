078f4ba4ab50f03f539f41606a642d4902df6259
dawitkiros <dawitkiros1922@gmail.com>
2022-06-28 13:24:15 -0700

    update

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 for ch in range(97, 123):
-<<<<<<< HEAD
 	print("{:c}".format(ch), end='')
-=======
-    print("{:c}".format(ch), end='')
->>>>>>> 0a044166ebeae64058cbc140db19b051da715ed5
