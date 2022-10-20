00e6882ca5c0f8c6dddccd24625d8bff619d0251
Dawit Kiros <101798935+Dawitkiros@users.noreply.github.com>
2022-10-20 22:55:25 +0530

    update

---
+++ 0x10-python-network_0/2-delete.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# sends a DELETE request to the URL passed as the first argument and display the body of the response
+curl -sX DELETE "$1"
---
+++ 0x10-python-network_0/6.peak.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""contains the function find_peak"""
+def find_peack(list_of_integers):
+    """finds a peak in all of the unsorted integers"""
+    li = list_of_integers
+    l = len(li)
+    if l == 0:
+        return
+    m= l//2
+    if (m == l-1 or li[m] >=li[m+1]) and (m == 0 or li[m] >= li[m-1]):
+        return li[m]
+    if m !=l-1 and li[m+1]>li[m]:
+        return find_peak(li[m+1:])
+    return find_peak(li[:m])
